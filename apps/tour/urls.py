6584c0ca50474d3435ce2f72fa29abe9219af26d
adylbek0408 <adylbek0408@icloud.com>
2025-02-17 00:06:45 +0600

    add url router package dtl

--- apps/tour/urls.py
+++ apps/tour/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     AjyViewSet, CategoryPackageViewSet,
-    TourDateViewSet, PackageViewSet, HotelViewSet
+    TourDateViewSet, PackageViewSet, HotelViewSet, PackageDetailViewSet
 )
 
 router = DefaultRouter()
@@ -11,7 +11,7 @@ router.register('categories', CategoryPackageViewSet, basename='category')
 router.register('tour-dates', TourDateViewSet, basename='tour-date')
 router.register('packages', PackageViewSet, basename='package')
 router.register('hotels', HotelViewSet, basename='hotel')
-
+router.register('package-details', PackageDetailViewSet, basename='package-detail')
 urlpatterns = [
     path('', include(router.urls)),
 ]
