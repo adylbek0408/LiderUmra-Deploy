d924985b5deb3f7909b9769f49592c65626422e0
adylbek0408 <adylbek0408@icloud.com>
2025-02-25 01:33:11 +0600

    update admin blog

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -5,46 +5,46 @@ from modeltranslation.admin import TranslationAdmin
 
 
 
-# class DetailDescriptionInline(admin.StackedInline):
-#     model = DetailDescription
-#     extra = 1
-#     fieldsets = (
-#         ('Кыргызча', {
-#             'fields': ('blog', 'text_ky', 'image')
-#         }),
-#         ('Русский', {
-#             'fields': ('text_ru',)
-#         }),
-#     )
+class DetailDescriptionInline(admin.StackedInline):
+    model = DetailDescription
+    extra = 1
+    fieldsets = (
+        ('Кыргызча', {
+            'fields': ('blog', 'text_ky', 'image')
+        }),
+        ('Русский', {
+            'fields': ('text_ru',)
+        }),
+    )
 
 
-# @admin.register(Blog)
-# class BlogAdmin(admin.ModelAdmin):
-#     inlines = [DetailDescriptionInline]
-#     fieldsets = (
-#         ('Кыргызча', {
-#             'fields': ('title_ky', 'rich_ky', 'image',)
-#         }),
-#         ('Русский', {
-#             'fields': ('title_ru', 'rich_ru',)
-#         }),
-#     )
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    inlines = [DetailDescriptionInline]
+    fieldsets = (
+        ('Кыргызча', {
+            'fields': ('title_ky', 'rich_ky', 'image',)
+        }),
+        ('Русский', {
+            'fields': ('title_ru', 'rich_ru',)
+        }),
+    )
 
 
-# @admin.register(Lesson)
-# class LessonAdmin(admin.ModelAdmin):
-#     list_display = ['title_ky', 'title_ru', 'created_at']
-#     search_fields = ['title_ky', 'title_ru', 'rich_ky', 'rich_ru']
-#     list_filter = ['created_at']
-#     readonly_fields = ['created_at']
-#     fieldsets = (
-#         ('Кыргызча', {
-#             'fields': ('title_ky', 'rich_ky', 'video_url')
-#         }),
-#         ('Русский', {
-#             'fields': ('title_ru', 'rich_ru')
-#         }),
-#     )
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title_ky', 'title_ru', 'created_at']
+    search_fields = ['title_ky', 'title_ru', 'rich_ky', 'rich_ru']
+    list_filter = ['created_at']
+    readonly_fields = ['created_at']
+    fieldsets = (
+        ('Кыргызча', {
+            'fields': ('title_ky', 'rich_ky', 'video_url')
+        }),
+        ('Русский', {
+            'fields': ('title_ru', 'rich_ru')
+        }),
+    )
 
 
 @admin.register(FAQ)
